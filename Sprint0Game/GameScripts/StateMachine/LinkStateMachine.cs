68bf9dc216cd762d8de926872bcc4dd5393c8805
simplerhamm <118569188+simplerhamm@users.noreply.github.com>
2026-09-24 21:59:18 -0400

    LinkStateMachine.cs

---
+++ Sprint0Game/GameScripts/StateMachine/LinkStateMachine.cs
@@ -0,0 +1,50 @@
+﻿using Sprint0Game.GameScripts.Interfaces;
+using Sprint0Game.GameScripts.StateMachine.LinkStates;
+
+namespace Sprint0Game.GameScripts.StateMachine
+{
+    public class LinkStateMachine
+    {
+        private ILinkState state;
+
+        public LinkStateMachine(IPlayer link)
+        {
+            state = new IdleLinkState(link);
+        }
+
+        public void UpdateState(ILinkState state)
+        {
+            this.state = state;
+        }
+
+        public void TakeDamage()
+        {
+            state.TakeDamage();
+        }
+
+        public void Move()
+        {
+            state.Move();
+        }
+
+        public void Attack()
+        {
+            state.Attack();
+        }
+
+        public void UseItem()
+        {
+            state.UseItem();
+        }
+
+        public void Interact()
+        {
+            state.Interact();
+        }
+
+        public void Heal()
+        {
+            state.Heal();
+        }
+    }
+}
