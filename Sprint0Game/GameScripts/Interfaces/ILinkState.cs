dd737c6c4ba6751fdbe7665138d19022d0d7010e
simplerhamm <118569188+simplerhamm@users.noreply.github.com>
2026-09-24 21:39:34 -0400

    ILinkState.cs, folders for States, and IdleLinkState.cs

---
+++ Sprint0Game/GameScripts/Interfaces/ILinkState.cs
@@ -0,0 +1,12 @@
+﻿namespace Sprint0Game.GameScripts.Interfaces
+{
+    public interface ILinkState
+    {
+        void TakeDamage();
+        void Move();
+        void Attack();
+        void UseItem();
+        void Interact();
+        void Heal();
+    }
+}
---
+++ Sprint0Game/GameScripts/StateMachine/LinkStates/IdleLinkState.cs
@@ -0,0 +1,51 @@
+﻿using Sprint0Game.GameScripts.Interfaces;
+
+namespace Sprint0Game.GameScripts.StateMachine.LinkStates
+{
+    public class IdleLinkState : ILinkState
+    {
+        private IPlayer link;
+        private ICommand command;
+
+        public IdleLinkState(IPlayer link)
+        {
+            this.link = link;
+        }
+
+        public void TakeDamage()
+        {
+            // command = new LinkTakeDamageCommand();
+            // command.Execute();
+        }
+
+        public void Move()
+        {
+            // command = new LinkMoveCommand();
+            // command.Execute();
+        }
+
+        public void Attack()
+        {
+            // command = new LinkAttackCommand();
+            // command.Execute();
+        }
+
+        public void UseItem()
+        {
+            // command = new LinkUseItemCommand();
+            // command.Execute();
+        }
+
+        public void Interact()
+        {
+            // command = new LinkInteractCommand();
+            // command.Execute();
+        }
+
+        public void Heal()
+        {
+            // command = new LinkHealCommand();
+            // command.Execute();
+        }
+    }
+}
